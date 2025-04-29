761d09adf51e9554c5975571f3e5f5bc0da87067
erste-websites <erste.websites@gmail.com>
2025-04-29 11:57:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -35,3 +35,21 @@ function checkout() {
     alert('Your cart is empty!');
   }
 }
+
+// Initialize an empty cart in localStorage if it's not already there
+if (!localStorage.getItem('cart')) {
+    localStorage.setItem('cart', JSON.stringify([]));
+}
+
+// Function to add product to the cart
+function addToCart(productName, productPrice) {
+    let cart = JSON.parse(localStorage.getItem('cart'));
+    cart.push({ name: productName, price: productPrice });
+    localStorage.setItem('cart', JSON.stringify(cart));
+
+    alert(`${productName} has been added to your cart!`);
+}
+
+// Example of how to link the Add to Cart button in HTML
+// <button class="btn" onclick="addToCart('Crystal Chandelier', 2500)">Add to Cart</button>
+
